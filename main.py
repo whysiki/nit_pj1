11cd1bf20ccf3eb77cce22818decc241dfb3cbc3
whysiki <whysiki@proton.me>
2024-12-07 16:08:45 +0800

    1

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from main_funcs import *
 from playwright_pj_url_async import pj_urls as pj_urls_async
+import asyncio
 
 if __name__ == "__main__":
     # 设置学期
     semester = "2024-2025-1"
     aim_status = None
-    sigle_pj_urls = get_single_pj_urls(semester=semester, aim_status=aim_status)
+    sigle_pj_urls = get_single_pj_urls(semester=semester, aim_status=aim_status)  # type: ignore
     if sigle_pj_urls:
         # pj_urls_async(sigle_pj_urls)
-        asyncio.run(pj_urls_async(sigle_pj_urls, headless=False))
+        asyncio.run(pj_urls_async(headless=False))
     sigle_pj_urls2 = get_single_pj_urls(semester=semester)
     assert not sigle_pj_urls2, "There are still some courses need to be evaluated"
     print("[green]All courses have been evaluated[/green]")
--- playwright_pj_url_async.py
+++ playwright_pj_url_async.py
@@ -5,12 +5,14 @@ import os
 import urllib
 import random
 from functools import wraps
-import asyncio
 from rich import print
-
+import re
 from playwright_stealth import stealth_async
+from pathlib import Path
+import asyncio
 
 # from undetected_chromedriver import Chrome, ChromeOptions
+import json
 
 # Load environment variables
 load_dotenv(".env")
@@ -25,8 +27,8 @@ assert (
 
 
 def unquote_urlparams(urlparams: str) -> dict:
-    urlparams = urllib.parse.unquote(urlparams)
-    urlparams = urlparams.split("?")[1].split("&")
+    urlparams = urllib.parse.unquote(urlparams)  # type: ignore
+    urlparams = urlparams.split("?")[1].split("&")  # type: ignore
     return {
         param.split("=")[0]: (param.split("=")[1] if len(param.split("=")) > 1 else "")
         for param in urlparams
@@ -49,93 +51,182 @@ def retry_decorator(func):
 
 # @retry_decorator
 async def run_single(single_url: str, page: Page) -> None:
-    print(f"Evaluating: {single_url}")
+    try:
+        print(f"Evaluating: {single_url}")
+        url_params = unquote_urlparams(single_url)
+        kcmc = url_params["kcmc"]  # 课程名称
+        jgxm = url_params["jgxm"]  # 教工姓名
+        xnxqid = url_params["xnxqid"]  # 学年学期ID
+        print(f"学年学期ID: {xnxqid},课程名称: {kcmc}, 教工姓名: {jgxm}")
+        await page.evaluate(f"window.open('{single_url}')")  # 打开新标签页
+        await page.wait_for_selector("div.evaluation-btns", timeout=60000)
+        evaluation_buttons_container = await page.query_selector("div.evaluation-btns")
+        if evaluation_buttons_container:
+            await page.wait_for_selector("tr.el-table__row")
+            rows = await page.query_selector_all("tr.el-table__row")
+            # 设置为B的评价
+            B_index = random.randint(0, len(rows) - 1)
+            for index, row in enumerate(rows):
+                print(
+                    f"index: {index}, B_index: {B_index}, row: {(await row.inner_text()).replace(' ', '').replace('\n', '')}"
+                )
+                labels = await row.query_selector_all("label[role='radio']")  # ABCD
+                random_label = labels[0] if index != B_index else labels[1]
+                input_span = await random_label.query_selector("span.el-radio__label")
+                while not await random_label.is_visible():
+                    print(f"index: {index}, B_index: {B_index}, not visible")
+                    await page.wait_for_timeout(1000)
+                await random_label.scroll_into_view_if_needed()
+                await random_label.click()
+                if input_span:
+                    print(await input_span.inner_text())
+                else:
+                    print(f"index: {index}, B_index: {B_index}, No input_span")
+            await page.locator("textarea").click()
+            await page.locator("textarea").fill("好")
+            await page.get_by_role("button", name="确认").click()
+            await page.wait_for_selector("div.el-textarea")
+            await page.wait_for_selector("button")
+            affirm_pop_windows = page.get_by_text("提示确认提交? 取消 确定")
+            if affirm_pop_windows:
+                affirm_buttons = affirm_pop_windows.locator("button", has_text="确定")
+                if affirm_buttons:
+                    await affirm_buttons.click()
+                else:
+                    raise Exception("No affirm_buttons")
+            else:
+                raise Exception("No affirm_pop_windows")
+        else:
+            raise Exception("No message_box")
+    except Exception as e:
+        with open("cache/error.html", "w", encoding="utf-8", errors="ignore") as f:
+            f.write(await page.content())
+        await page.pause()
+    finally:
+        pass
 
-    url_params = unquote_urlparams(single_url)
-    # print(url_params)
-    kcmc = url_params["kcmc"]  # 课程名称
-    jgxm = url_params["jgxm"]  # 教工姓名
-    xnxqid = url_params["xnxqid"]  # 学年学期ID
-    print(f"学年学期ID: {xnxqid},课程名称: {kcmc}, 教工姓名: {jgxm}")
-    current_referer = page.url
-    print(current_referer)
-    # await page.goto(single_url, referer=current_referer)
-    # 打开窗口
-    await page.evaluate(f"window.open('{single_url}')")
-    await page.wait_for_selector("tr.el-table__row")
 
-    message_box = await page.query_selector("div.el-message-box__btns")
-    if message_box:
-        rows = await page.query_selector_all("tr.el-table__row")
-        for row in rows:
-            labels = await row.query_selector_all("label[role='radio']")
-            random_label = labels[random.randint(0, 1)]
-            input_span = await random_label.query_selector("span.el-radio__label")
-            await random_label.click()
-            print(await input_span.inner_text())
-        await page.locator("textarea").click()
-        await page.locator("textarea").fill("好")
-        await page.get_by_role("button", name="确认").click()
-        await page.wait_for_selector("div.el-message-box__btns")
-        await page.wait_for_selector("button")
-        buttons = await message_box.query_selector_all("button")
-        for button in buttons:
-            if await button.inner_text() == "确定":
-                await button.click()
-                await page.wait_for_timeout(1000)
-        print(
-            f"[green]学年学期ID: {xnxqid}, 课程名称: {kcmc}, 教工姓名: {jgxm} Already evaluated[/green]"
-        )
-    else:
-        print(
-            f"[yellow]学年学期ID: {xnxqid}, 课程名称: {kcmc}, 教工姓名: {jgxm} Already evaluated[/yellow]"
-        )
-
-
-async def pj_urls(single_urls: list, headless: bool) -> None:
-    # save-browser-state
+async def pj_urls(headless: bool) -> None:
     state_path = "cache/brower_state.json"
     os.makedirs(os.path.dirname(state_path), exist_ok=True)
     async with async_playwright() as playwright:
-        browser: Browser = await playwright.firefox.launch(headless=headless)
+        browser: Browser = await playwright.chromium.launch(headless=headless)
         async with browser:
-            # if not os.path.exists(state_path):
-            if True:
-                context: BrowserContext = await browser.new_context()
-                page1: Page = await context.new_page()
-                if headless:
-                    await stealth_async(page1)
-                await page1.goto(
-                    "http://portal.nit.edu.cn/index#/app/home/main",
-                    wait_until="domcontentloaded",
-                )
+            context: BrowserContext = (
+                await browser.new_context()
+                if not os.path.exists(state_path)
+                else await browser.new_context(storage_state=state_path)
+            )
+            page1: Page = await context.new_page()
+            if headless:
+                await stealth_async(page1)
+            await page1.goto(
+                "http://portal.nit.edu.cn/index#/app/home/main",
+                wait_until="domcontentloaded",
+            )
+            if not os.path.exists(state_path):
                 await page1.wait_for_selector("input[placeholder='Username']")
-                await page1.get_by_placeholder("Username").fill(USER_NAME)
-                await page1.get_by_placeholder("Password").fill(PASSWORD)
+                await page1.get_by_placeholder("Username").fill(USER_NAME)  # type: ignore
+                await page1.get_by_placeholder("Password").fill(PASSWORD)  # type: ignore
                 await page1.get_by_role("button", name="LOGIN").click()
-                async with page1.expect_popup(timeout=600000) as page2_info:
-                    await page1.get_by_text("教务管理系统").click()
-                page2 = await page2_info.value
-                async with page2.expect_popup(timeout=600000) as page3_info:
-                    await page2.wait_for_selector("div.menu-one-label")
-                    await page2.locator("div.menu-one-label").filter(
-                        has_text="学生评教"
-                    ).click(click_count=2, delay=1000)
-                page3 = await page3_info.value
-                await page3.get_by_text("阶段评价").click()
-                await page3.wait_for_selector(
-                    "td.el-table_1_column_9.is-center.el-table__cell > div > button"
+            async with page1.expect_popup(timeout=600000) as page2_info:
+                await page1.get_by_text("教务管理系统").click()
+            # await page1.close()
+            page2 = await page2_info.value
+            async with page2.expect_popup(timeout=600000) as page3_info:
+                await page2.wait_for_selector("div.menu-one-label")
+                await page2.locator("div.menu-one-label").filter(
+                    has_text="学生评教"
+                ).click(click_count=2, delay=1000)
+            # await page2.close()
+            page = await page3_info.value
+            await page.get_by_text("阶段评价").click()
+            await page.wait_for_selector(
+                "td.el-table_1_column_9.is-center.el-table__cell > div > button"
+            )
+            buttons = await page.query_selector_all(
+                "td.el-table_1_column_9.is-center.el-table__cell > div > button"
+            )
+            print(f"找到{len(buttons)}个课程类型")
+            original_url_main = page.url
+            print(f"original_url_main: {original_url_main}")
+            # http://jiaowupj.nit.edu.cn/evaluate/studentEvaluate/student-jdp/index
+            tasksjson: dict = json.loads(Path("tasks.json").read_text(encoding="utf-8"))
+            # 关闭之前的所有page
+            for page_ in context.pages:
+                if page_ != page:
+                    await page_.close()
+
+            single_urls = []
+            tasks = []
+            for item in tasksjson.get("items", []):
+                taskname = item["taskname"]
+                print(f"taskname: {taskname}")
+                semester = re.search(r"\d{4}-\d{4}-\d", item["taskname"])
+                if semester:
+                    semester = semester.group()
+                else:
+                    raise Exception("No semester")
+                pjbuttons_url = f"http://jiaowupj.nit.edu.cn/evaluate/studentEvaluate/student-jdp/two-index?taskid={item['taskid']}&xnxq={semester}"
+                print(f"pjbuttons_url: {pjbuttons_url}")
+                await page.goto(pjbuttons_url)
+
+                pjbutton_selector = "#myTableWrapper > div > div > div > table > tbody > tr > td > div > button"
+                await page.wait_for_selector(pjbutton_selector)
+                pjbuttons = page.locator(
+                    pjbutton_selector,
+                    has_not_text="查看",
                 )
-                button = await page3.query_selector(
-                    "td.el-table_1_column_9.is-center.el-table__cell > div > button"
-                )
-                await button.click()
-                await context.storage_state(path=state_path)
-            else:
-                pass
-                # context: BrowserContext = await browser.new_context(
-                #     storage_state=state_path
-                # )
-                # page3: Page = await context.new_page()
-            async with context:
-                await asyncio.gather(*[run_single(url, page3) for url in single_urls])
+                pjbutton_count = await pjbuttons.count()
+                print(f"找到{pjbutton_count}个课程评价按钮")
+                # original_url = page.url
+
+                if pjbutton_count:
+                    for i in range(pjbutton_count):
+
+                        async def task(i, page: Page):
+                            try:
+                                await page.goto(pjbuttons_url)  # 回到二级页面
+                                print(f"第{i+1}个课程评价按钮")
+                                pjbutton = page.locator(
+                                    pjbutton_selector,
+                                    has_not_text="查看",
+                                )
+                                if pjbutton:
+                                    # 获取按钮的父级 div 元素
+                                    pjbutton = pjbutton.first
+                                    parent_div = pjbutton.locator(
+                                        ".."
+                                    )  # ".." 选择父元素
+                                    parent_html = await parent_div.inner_html()
+                                    print(f"父级 div HTML: {parent_html}")
+
+                                    await parent_div.click()
+                                    await page.wait_for_selector(
+                                        "div.lecture-evaluation-header > h2"
+                                    )
+                                    nev_url = page.url
+                                    # print(f"nev_url: {nev_url}")
+                                    single_urls.append(nev_url)
+                                    # await run_single(nev_url, page)
+                                    # print(f"return to original_url: {page.url}")
+                                else:
+                                    print("No pjbutton")
+                            finally:
+                                await page.close()
+
+                        tasks.append(task(i, (await context.new_page())))
+                else:
+                    print("No pjbuttons")
+            await asyncio.gather(*tasks)
+            await context.storage_state(path=state_path)
+
+            # 关闭所有页面
+            for page_ in context.pages:
+                if page_ != page:
+                    await page_.close()
+
+            # 评教
+            await asyncio.gather(
+                *[run_single(single_url, page) for single_url in single_urls]
+            )
