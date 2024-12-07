58e0ed65af9e6d643e8c6e5dcf86258741f2da19
whysiki <whysiki@proton.me>
2024-12-07 16:09:36 +0800

    1

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     if sigle_pj_urls:
         # pj_urls_async(sigle_pj_urls)
         asyncio.run(pj_urls_async(headless=False))
-    sigle_pj_urls2 = get_single_pj_urls(semester=semester)
-    assert not sigle_pj_urls2, "There are still some courses need to be evaluated"
-    print("[green]All courses have been evaluated[/green]")
+        sigle_pj_urls2 = get_single_pj_urls(semester=semester)
+        assert not sigle_pj_urls2, "There are still some courses need to be evaluated"
+        print("[green]All courses have been evaluated[/green]")
     jsons_ = Path(__file__).parent.glob("*.json")
     for json_ in jsons_:
         json_.unlink()
